f5870dca0c4a8598a622ff3f3de9dce783990e8a
Austin <austinm@ubuntu.(none)>
2013-02-24 19:09:52 -0800

    renamed program

--- src/main.c
+++ src/main.c
@@ -4,6 +4,12 @@
 
 int main(int argc, char* argv[]) {
 
+   if (argc < 3) {
+      printf("Usage: md5cracker dictionary-file hash\n");
+      return EXIT_FAILURE;
+   }
+
+
 
    return EXIT_SUCCESS;
 }
