12deed43e047361b74dbe2f6f79b7d84a2ae89e3
systmkor <systmkor@gmail.com>
2013-03-09 14:54:38 -0800

    CPU Automated Compiling working.
    Makefile now autogenerates dependencies and compiles cpu md5 cracker.
    Modified md5 to work with cpu compile since __device__ I think i strictly
    for CUDA compilation with nvcc. Moved main.c to md5cracker.c for
    auto-magic makefile stuff.

--- include/md5.h
+++ include/md5.h
@@ -77,13 +77,13 @@ extern "C"
 #endif
 
 /* Initialize the algorithm. */
-__device__ void md5_init(md5_state_t *pms);
+void md5_init(md5_state_t *pms);
 
 /* Append a string to the message. */
-__device__ void md5_append(md5_state_t *pms, const md5_byte_t *data, int nbytes);
+void md5_append(md5_state_t *pms, const md5_byte_t *data, int nbytes);
 
 /* Finish the message and return the digest. */
-__device__ void md5_finish(md5_state_t *pms, md5_byte_t digest[16]);
+void md5_finish(md5_state_t *pms, md5_byte_t digest[16]);
 
 #ifdef __cplusplus
 }  /* end extern "C" */
